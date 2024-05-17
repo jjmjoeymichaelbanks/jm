f304ba5b6729244ac83e50134cd8eafc2b8dad80
Joey Michael Banks <acktic@outlook.com>
2024-05-17 09:39:24 -0700

    Update Assets.js

--- site/js/main/Assets.js
+++ site/js/main/Assets.js
@@ -24,8 +24,8 @@
        userName: https://www.youtube.com/feeds/videos.xml?user=
        channel: https://www.youtube.com/feeds/videos.xml?channel_id=
 var menu=[
-{id:`4Chan/Wallpapers`,category:`Local`,title:``,description:`Wallpapers General`,uri:`https://boards.4chan.org/wg/index.rss`,image:`4chan`,hash:`WP`,media:!1},
-{id:`4Chan/Technology`,category:`Local`,title:``,description:`G Technology`,uri:`https://boards.4chan.org/g/index.rss`,image:`4chan`,hash:`4G`,media:!0},
+{id:`Reddit/Wallpapers`,category:`Local`,title:``,description:`Wallpapers General`,uri:`https://boards.4chan.org/wg/index.rss`,image:`4chan`,hash:`WP`,media:!1},
+{id:`Reddit/Technology`,category:`Local`,title:``,description:`G Technology`,uri:`https://boards.4chan.org/g/index.rss`,image:`4chan`,hash:`4G`,media:!0},
 ]
 */
 
