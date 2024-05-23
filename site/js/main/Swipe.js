7ff4b7fa96b896b0d211fd751cdcc6e3fa8292b2
Joey Michael Banks <acktic@outlook.com>
2024-05-23 01:53:59 -0700

    Add files via upload

---
+++ site/js/main/Swipe.js
@@ -0,0 +1,47 @@
+_container
+  .addEventListener(
+    'touchstart', (evt) =>
+    {
+      touchmove = false;
+      touchstartX = evt.changedTouches[0].screenX;
+  },
+  {
+    passive:
+      true
+  }
+);
+
+_container
+  .addEventListener(
+    'touchend', (evt) =>
+    {
+      if (
+        window.innerWidth > 425
+      )
+        Swipe();
+      touchendX = evt.changedTouches[0].screenX;
+      let isScrolling;
+
+      // Clear our timeout throughout the scroll
+      window.clearTimeout( isScrolling );
+
+      // Set a timeout to run after scrolling ends
+      isScrolling =
+        setTimeout(
+          function() {
+
+            // Run the callback
+            touchmove = true;
+
+          },
+        4000);
+    if (
+      _guide.style.display != `flex`
+    )
+      Swipe();
+  },
+  {
+    passive:
+    true
+  }
+);
